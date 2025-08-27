a5678462f0bce9d89abe99fa598bc54663c6b6b9
Gaurav <patelgaurav1202@gmail.com>
2025-08-27 19:02:15 +0530

    I18nkey add

--- src/lib/I18nKey.ts
+++ src/lib/I18nKey.ts
@@ -1,5 +1,100 @@
-enum I18nKey {
-    FEATURES = 'react.features',
-}
-
+const I18nKey = {
+  // Navigation
+  FEATURES: 'react.features',
+  USE_CASES: 'react.use_cases',
+  PRICING: 'react.pricing',
+  ABOUT: 'react.about',
+  FAQ: 'react.faq',
+  SIGN_IN: 'react.sign_in',
+  GET_STARTED: 'react.get_started',
+  
+  // Hero section
+  BADGE_NEW_WORKFLOW: 'react.badge_new_workflow',
+  HEADLINE: 'react.headline',
+  SUBHEADLINE: 'react.subheadline',
+  START_FREE: 'react.start_free',
+  SEE_FEATURES: 'react.see_features',
+  
+  // Section 2
+  SECTION2_HEADLINE: 'react.section2_headline',
+  SECTION2_SUBHEADLINE: 'react.section2_subheadline',
+  GET_STARTED_NOW: 'react.get_started_now',
+  
+  // Model categories
+  GPT_MODELS: 'react.gpt_models',
+  GPT_MODELS_DESC: 'react.gpt_models_desc',
+  FAST_MODELS: 'react.fast_models',
+  FAST_MODELS_DESC: 'react.fast_models_desc',
+  CREATIVE_MODELS: 'react.creative_models',
+  CREATIVE_MODELS_DESC: 'react.creative_models_desc',
+  ANALYTICAL_MODELS: 'react.analytical_models',
+  ANALYTICAL_MODELS_DESC: 'react.analytical_models_desc',
+  
+  // YouTube section
+  YOUTUBE_HEADLINE: 'react.youtube_headline',
+  YOUTUBE_SUBHEADLINE: 'react.youtube_subheadline',
+  YOUTUBE_DESCRIPTION: 'react.youtube_description',
+  YOUTUBE_BULLET1: 'react.youtube_bullet1',
+  YOUTUBE_BULLET2: 'react.youtube_bullet2',
+  YOUTUBE_BULLET3: 'react.youtube_bullet3',
+  YOUTUBE_BULLET4: 'react.youtube_bullet4',
+  SUBSCRIBE_CHANNEL: 'react.subscribe_channel',
+  JOIN_ENTHUSIASTS: 'react.join_enthusiasts',
+  SUPPORT_MISSION: 'react.support_mission',
+  
+  // AI Models
+  PICK_MODELS: 'react.pick_models',
+  CHATGPT5: 'react.chatgpt5',
+  CHATGPT5_SUB: 'react.chatgpt5_sub',
+  CHATGPT5_DESC: 'react.chatgpt5_desc',
+  CLAUDE4: 'react.claude4',
+  CLAUDE4_SUB: 'react.claude4_sub',
+  CLAUDE4_DESC: 'react.claude4_desc',
+  GEMINI25: 'react.gemini25',
+  GEMINI25_SUB: 'react.gemini25_sub',
+  GEMINI25_DESC: 'react.gemini25_desc',
+  PERPLEXITY: 'react.perplexity',
+  PERPLEXITY_SUB: 'react.perplexity_sub',
+  PERPLEXITY_DESC: 'react.perplexity_desc',
+  DEEPSEEK: 'react.deepseek',
+  DEEPSEEK_SUB: 'react.deepseek_sub',
+  DEEPSEEK_DESC: 'react.deepseek_desc',
+  GROK4: 'react.grok4',
+  GROK4_SUB: 'react.grok4_sub',
+  GROK4_DESC: 'react.grok4_desc',
+  
+  // About section
+  ABOUT_TEAM: 'react.about_team',
+  OUR_MISSION: 'react.our_mission',
+  OUR_VALUES: 'react.our_values',
+  MISSION_TEXT: 'react.mission_text',
+  FOUNDED_TEXT: 'react.founded_text',
+  VALUE1: 'react.value1',
+  VALUE2: 'react.value2',
+  VALUE3: 'react.value3',
+  VALUE4: 'react.value4',
+  VALUE5: 'react.value5',
+  
+  // FAQ section
+  FAQ_TITLE: 'react.faq_title',
+  FAQ1: 'react.faq1',
+  FAQ2: 'react.faq2',
+  FAQ3: 'react.faq3',
+  FAQ4: 'react.faq4',
+  FAQ5: 'react.faq5',
+  
+  // Footer
+  DONATE: 'react.donate',
+  SIGN_UP: 'react.sign_up',
+  COPYRIGHT: 'react.copyright',
+  
+  // Dashboard
+  NEW_CHAT: 'react.new_chat',
+  PROJECTS: 'react.projects',
+  FREE_PLAN: 'react.free_plan',
+  MESSAGES_USED: 'react.messages_used',
+  UPGRADE_PLAN: 'react.upgrade_plan',
+  SETTINGS: 'react.settings',
+  ASK_MODELS: 'react.ask_models'
+};
 export default I18nKey;
